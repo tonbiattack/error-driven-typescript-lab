14b7a9722912af2c480d7361e3de49f09bb9b0a8
tonbiattack <4.0497724e+07+tonbiattack@users.noreply.github.com>
2026-08-13 09:57:42 +0000

    有効な利用者の検索条件を返す

--- src/callbacks/findActiveUser.ts
+++ src/callbacks/findActiveUser.ts
@@ -4,7 +4,5 @@ export interface User {
 }
 
 export function findActiveUser(users: readonly User[], id: string): User | undefined {
-  return users.find((user) => {
-    user.id === id && user.active;
-  });
+  return users.find((user) => user.id === id && user.active);
 }
